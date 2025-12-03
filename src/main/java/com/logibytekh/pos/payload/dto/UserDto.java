dfdbd53a0c89896e7aa8e6357f67695b57fe3ec1
codebyme(PANHA) <152833521+PanhaPhin@users.noreply.github.com>
2025-12-03 19:47:22 +0700

    commit

--- src/main/java/com/logibytekh/pos/payload/dto/UserDto.java
+++ src/main/java/com/logibytekh/pos/payload/dto/UserDto.java
@@ -23,7 +23,7 @@ public class UserDto {
 
     private String phone;
 
-    private String address;
+   
 
    
     private String password;
--- src/main/java/com/logibytekh/pos/payload/response/ApiResponse.java
+++ src/main/java/com/logibytekh/pos/payload/response/ApiResponse.java
@@ -1,8 +1,12 @@
 package com.logibytekh.pos.payload.response;
 
+import com.logibytekh.pos.payload.dto.UserDto;
+
 public class ApiResponse {
     private String jwt;
     private String message;
+
+    private UserDto user;
 
     
 }
